80e72b7c0dc5590875b3a5c30db830ff419338c8
bats314159 <104912784+bats314159@users.noreply.github.com>
2026-03-24 22:54:57 -0400

    Add constants.js with application constants

---
+++ constants.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Application constants
+const APP_NAME = 'Base Project';
+const VERSION = '1.0.0';
+const AUTHOR = 'bats314159';
+const CREATED_AT = '2026-03-25 02:52:59';
+
+module.exports = {
+    APP_NAME,
+    VERSION,
+    AUTHOR,
+    CREATED_AT,
+};
